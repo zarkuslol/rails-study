bce67ec550b36a4c3a2046643529070028b9cf63
zarkuslol <lucasferlima8@gmail.com>
2023-07-19 15:42:44 -0300

    Testing Git config

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,6 @@
 class MainController < ApplicationController
   def index
     flash[:notice] = 'Logged in successfully'
-    flash[:alert] = 'Invalid email or password'
+    flash[:alert] = 'Invalid email and/or password'
   end
 end
